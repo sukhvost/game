1b916b06d6405822de4b55e1cf42e61dfa951cdd
Melnikov Viktor <sukhvost@yandex.ru>
2020-04-24 14:03:12 +0300

    add structure program

--- static/my-jq.js
+++ static/my-jq.js
@@ -1,33 +1,50 @@
 window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
-
-    // let num1 = Math.floor(Math.random() * 3);
-    // let num2 = Math.floor(Math.random() * 3);
-
-    let score = 0;
-    let btns = document.querySelectorAll('button');
-
-
-
+    let counter = 0;
+    let start = document.querySelector('.wrap');
     let massArg = ['Камень', 'Ножницы', 'Бумага'];
+    let gamer1 = 0;
+    let gamer2 = 0;
+    start.addEventListener('click', function(e) {
+        // console.log(e)
+        let target = e.target;
+
+        if (target.innerText == 'gamer 1') {
+            console.log(getArg(massArg));
+            gamer1 = counter;
+            target.disabled = true;
+
+        }
+        if (target.innerText == 'gamer 2') {
+            console.log(getArg(massArg));
+            gamer2 = counter;
+            target.disabled = true;
+        }
+        if (target.innerText == 'result') {
+            getResult(gamer1, gamer2);
+            target.disabled = true;
+        }
+
+    });
+
+    function getResult(a, b) {
+        if (a < b && b != 2 || a == 1 && b == 2 || a == 2 && b == 0) {
+            console.log('win 1');
+        } else if (a == b) {
+            console.log('nobody');
+        } else {
+            console.log('win 2');
+        }
+    }
 
     function getRandNum() {
-        return Math.floor(Math.random() * 3);
+        let num = Math.floor(Math.random() * 3);
+        return num;
     }
-
 
     function getArg(arg) {
-        let randNum = getRandNum();
-        return arg[randNum];
+        counter = getRandNum();
+        return arg[counter];
     }
-    let randArg = getArg(massArg);
-
-    function getScore() {
-
-    }
-
-    console.log(randArg);
-
-
 });
