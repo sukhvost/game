95b81e6d8a3cf2177c3bfa7216ac6bfe4d9e1045
Melnikov Viktor <sukhvost@yandex.ru>
2020-04-25 15:17:41 +0300

    added score and description-block

--- static/my-jq.js
+++ static/my-jq.js
@@ -3,67 +3,73 @@ window.addEventListener('DOMContentLoaded', function() {
     'use strict';
     let counter = 0;
     let start = document.querySelector('.wrap');
-    let massArg = ['Камень', 'Ножницы', 'Бумага'];
     let massImg = ['0.png', '1.png', '2.png'];
     let gamer1 = 0;
     let gamer2 = 0;
-    let scoreg = 1;
-    let score1 = document.querySelector('.score_1 span');
-    let score2 = document.querySelector('.score_2 span');
+    let btns = document.querySelectorAll('.gamer button');
+    let scoreGamer1 = 0;
+    let scoreGamer2 = 0;
+    let elemScore1 = document.querySelector('.score_1 span');
+    let elemScore2 = document.querySelector('.score_2 span');
     start.addEventListener('click', function(e) {
         // console.log(e)
         let target = e.target;
 
-        if (target.innerText == 'gamer 1') {
-            console.log(getArg(massArg));
-            let elem = document.querySelector('.card_first_gamer img');
-            showImg(elem, massImg, counter);
-            gamer1 = counter;
-            // target.disabled = true;
+        if (target.innerText == 'Start') {
+            document.querySelector('.intro').style.display = 'none';
+            document.querySelector('.game').style.display = 'block';
+        }
 
+        if (target.innerText == 'gamer 1') {
+            let elem = document.querySelector('.card_first_gamer img');
+            showImg(elem, massImg);
+            gamer1 = counter;
+            target.disabled = true;
         }
         if (target.innerText == 'gamer 2') {
-            console.log(getArg(massArg));
             let elem = document.querySelector('.card_second_gamer img');
-            showImg(elem, massImg, counter);
+            showImg(elem, massImg);
             gamer2 = counter;
-            // target.disabled = true;
+            target.disabled = true;
         }
         if (target.innerText == 'result') {
             getResult(gamer1, gamer2);
-            // target.disabled = true;
+            btns.forEach(function(item) {
+                item.disabled = false;
+            });
         }
-
     });
-
-    // выводим результаат на страницу
+    // выводит счет на страницу
+    function showScore(elem, score) {
+        elem.innerText = score;
+    }
+    // выводит результат на страницу
     function getResult(a, b) {
         let outResult = document.querySelector('.result p');
         outResult.classList.add('decor');
         if (a < b && b != 2 || a == 1 && b == 2 || a == 2 && b == 0) {
+            scoreGamer1++;
+            showScore(elemScore1, scoreGamer1);
             outResult.innerText = 'Победил игрок номер 1!';
         } else if (a == b) {
             outResult.innerText = 'У Вас ничья, Господа!';
         } else {
+            scoreGamer2++;
+            showScore(elemScore2, scoreGamer2);
             outResult.innerText = 'Победил игрок номер 2!';
         }
     }
 
-    // получаем случайное число от 0 до 3, так как наши массивы длинной 3
+    // генерирует случайное число от 0 до длинны массива, так как наши массивы длинной 3
     function getRandNum() {
-        let num = Math.floor(Math.random() * 3);
+        let num = Math.floor(Math.random() * massImg.length);
         return num;
     }
 
-    // получаем елемент массива
-    function getArg(arg) {
+    // выводит контент на страницу
+    function showImg(elem, imgs) {
         counter = getRandNum();
-        return arg[counter];
-    }
-
-    // устанавливаем контент
-    function showImg(elem, imgs, i) {
-        let img = imgs[i];
+        let img = imgs[counter];
         elem.setAttribute('src', `../img/${img}`);
     }
 });
